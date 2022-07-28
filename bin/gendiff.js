0038b1e53c10ac88f2a815b546aab4af0df880b2
Vladislav Sholokhov <vladislav.sholokhov@jetbrains.com>
2022-07-28 11:05:13 +0300

    complete the second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,5 +2,12 @@
 
 import {program} from "commander";
 
-program.description('Compares two configuration files and shows a difference.').version('1.0.0');
+program
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .helpOption('-h, --help','output usage information')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<filepath1> <filepath2>')
+
+
 program.parse(process.argv);
