e5778966313c3889be5832823132a7906b865dec
Vladislav Sholokhov <vladislav.sholokhov@jetbrains.com>
2022-07-28 10:38:59 +0300

    add cli command for gendiff

---
+++ bin/gendiff.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import {program} from "commander";
+
+program.description('Compares two configuration files and shows a difference.').version('1.0.0');
+program.parse(process.argv);
