d0bc2f1ff0e4cee3ddd85bac3d36b2e257c1a671
Vladislav Sholokhov <vladislav.sholokhov@jetbrains.com>
2022-08-24 10:53:00 +0300

    fourth step: add eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
-import {program} from "commander";
-import genDiff from "../src/index.js";
+import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .helpOption('-h, --help','output usage information')
-    .option('-f, --format <type>', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .action((file1, file2) => genDiff(file1, file2));
-
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((file1, file2) => genDiff(file1, file2));
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -2,38 +2,44 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 
-const getFilePath = ( file ) => path.resolve(process.cwd(), `fixtures`, file);
-const readFile = ( filePath ) => fs.readFileSync(filePath, 'utf-8');
+const getFilePath = (file) => path.resolve(process.cwd(), '__fixtures__', file);
+const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
-const genDiff = ( file1, file2 ) => {
-    const fileOneContent = JSON.parse(readFile(getFilePath(file1)));
-    const fileTwoContent = JSON.parse(readFile(getFilePath(file2)));
+const genDiff = (file1, file2) => {
+  const fileOneContent = JSON.parse(readFile(getFilePath(file1)));
+  const fileTwoContent = JSON.parse(readFile(getFilePath(file2)));
 
-    const keys1 = Object.keys(fileOneContent);
-    const keys2 = Object.keys(fileTwoContent);
-    const uniqSortedKeys = _.sortBy(_.uniq([...keys1, ...keys2]));
+  const keys1 = Object.keys(fileOneContent);
+  const keys2 = Object.keys(fileTwoContent);
+  const uniqSortedKeys = _.sortBy(_.uniq([...keys1, ...keys2]));
 
-    const result = uniqSortedKeys.map(( key ) => {
-        if (fileOneContent.hasOwnProperty(key) && !fileTwoContent.hasOwnProperty(key)) {
-            return ` - ${key}: ${fileOneContent[key]}\n`;
-        }
+  const resultArr = uniqSortedKeys.map((key) => {
+    const objOneHasKey = _.has(fileOneContent, key);
+    const objTwoHasKey = _.has(fileTwoContent, key);
 
-        if (!fileOneContent.hasOwnProperty(key) && fileTwoContent.hasOwnProperty(key)) {
-            return ` + ${key}: ${fileTwoContent[key]}\n`;
-        }
+    const objOneValue = fileOneContent[key];
+    const objTwoValue = fileTwoContent[key];
 
-        if (fileOneContent.hasOwnProperty(key) && fileTwoContent.hasOwnProperty(key)) {
-            if (fileOneContent[key] === fileTwoContent[key]) {
-                return `   ${key}: ${fileTwoContent[key]}\n`;
-            }
-            else {
-                return ` - ${key}: ${fileOneContent[key]}\n + ${key}: ${fileTwoContent[key]}\n`;
-            }
-        }
-    });
+    if (objOneHasKey && !objTwoHasKey) {
+      return ` - ${key}: ${objOneValue}\n`;
+    }
 
-    console.log(`{\n${result.toString().replaceAll(',', '')}}`);
+    if (!objOneHasKey && objTwoHasKey) {
+      return ` + ${key}: ${objTwoValue}\n`;
+    }
 
+    if (objOneHasKey && objTwoHasKey) {
+      if (objOneValue === objTwoValue) {
+        return `   ${key}: ${objOneValue}\n`;
+      }
+    }
+
+    return ` - ${key}: ${objOneValue}\n + ${key}: ${objTwoValue}\n`;
+  });
+
+  const resultStr = resultArr.toString().replaceAll(',', '');
+
+  console.log(`{\n${resultStr}}`);
 };
 
 export default genDiff;
