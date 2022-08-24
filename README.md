a204ff113179047c0dea261cc0d85cbfe515f4b3
Vladislav Sholokhov <vladislav.sholokhov@jetbrains.com>
2022-08-24 10:09:09 +0300

    third step

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/vladsholokhov/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/vladsholokhov/frontend-project-lvl2/actions)
+
+### Installation
+```
+make install
+make lint
+```
+
+### Compare .json files
+[![asciicast](https://asciinema.org/a/516814.svg)](https://asciinema.org/a/516814)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 import fs from 'fs';
 import path from 'path';
+import _ from 'lodash';
 
-const getFilePath = (file) => path.resolve(process.cwd(), `fixtures`, file);
-const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
+const getFilePath = ( file ) => path.resolve(process.cwd(), `fixtures`, file);
+const readFile = ( filePath ) => fs.readFileSync(filePath, 'utf-8');
 
-const genDiff = (file1, file2) => {
-    const fileOne = JSON.parse(readFile(getFilePath(file1)));
-    const fileTwo = JSON.parse(readFile(getFilePath(file2)));
+const genDiff = ( file1, file2 ) => {
+    const fileOneContent = JSON.parse(readFile(getFilePath(file1)));
+    const fileTwoContent = JSON.parse(readFile(getFilePath(file2)));
 
+    const keys1 = Object.keys(fileOneContent);
+    const keys2 = Object.keys(fileTwoContent);
+    const uniqSortedKeys = _.sortBy(_.uniq([...keys1, ...keys2]));
 
-    console.log(fileOne);
-    console.log(fileTwo);
+    const result = uniqSortedKeys.map(( key ) => {
+        if (fileOneContent.hasOwnProperty(key) && !fileTwoContent.hasOwnProperty(key)) {
+            return ` - ${key}: ${fileOneContent[key]}\n`;
+        }
+
+        if (!fileOneContent.hasOwnProperty(key) && fileTwoContent.hasOwnProperty(key)) {
+            return ` + ${key}: ${fileTwoContent[key]}\n`;
+        }
+
+        if (fileOneContent.hasOwnProperty(key) && fileTwoContent.hasOwnProperty(key)) {
+            if (fileOneContent[key] === fileTwoContent[key]) {
+                return `   ${key}: ${fileTwoContent[key]}\n`;
+            }
+            else {
+                return ` - ${key}: ${fileOneContent[key]}\n + ${key}: ${fileTwoContent[key]}\n`;
+            }
+        }
+    });
+
+    console.log(`{\n${result.toString().replaceAll(',', '')}}`);
+
 };
 
 export default genDiff;
