8412cb6ba679abb4cc2bdfe6c5d89b0cf99791fd
Vladislav Sholokhov <vladislav.sholokhov@jetbrains.com>
2022-08-24 13:16:56 +0300

    fourth step: fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,9 +6,6 @@ import genDiff from '../src/index.js';
 const getFilePath = (file) => path.resolve(process.cwd(), '__fixtures__', file);
 const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
-// const file1 = getFilePath('file1.json');
-// const file2 = getFilePath('file2.json');
-
 test('check plain json', () => {
   expect(genDiff('file1.json', 'file2.json')).toEqual(readFile(getFilePath('expected.txt')));
 });
