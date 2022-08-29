d115e03d0d0e41b931a2066bf6bd493813f29d51
Vladislav Sholokhov <vladislav.sholokhov@jetbrains.com>
2022-08-29 16:50:24 +0300

    sixth step: move diff generation to separate file, add basic formatter

---
+++ src/formatters/stylish.js
@@ -0,0 +1,5 @@
+const stylish = (tree) => {
+  console.log(tree);
+};
+
+export default stylish;
--- src/index.js
+++ src/index.js
@@ -1,50 +1,21 @@
 import fs from 'fs';
 import path from 'path';
-import _ from 'lodash';
 import parser from './parsers.js';
+import getDiff from './getDiff.js';
+import stylish from './formatters/stylish.js';
 
 const getFilePath = (file) => path.resolve(process.cwd(), '__fixtures__', file);
-const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
+const readFile = (filePath) => fs.readFileSync(filePath, { encoding: 'utf8' });
 
-const genDiff = (file1, file2) => {
+const genDiff = (file1, file2, format) => {
   const fileOnePath = getFilePath(file1);
   const fileTwoPah = getFilePath(file2);
   const fileOneContent = parser(readFile(fileOnePath), fileOnePath);
   const fileTwoContent = parser(readFile(fileTwoPah), fileTwoPah);
 
-  const keys1 = Object.keys(fileOneContent);
-  const keys2 = Object.keys(fileTwoContent);
-  const uniqSortedKeys = _.sortBy(_.uniq([...keys1, ...keys2]));
+  const diff = getDiff(fileOneContent, fileTwoContent);
 
-  const resultArr = uniqSortedKeys.map((key) => {
-    const objOneHasKey = _.has(fileOneContent, key);
-    const objTwoHasKey = _.has(fileTwoContent, key);
-
-    const objOneValue = fileOneContent[key];
-    const objTwoValue = fileTwoContent[key];
-
-    if (objOneHasKey && !objTwoHasKey) {
-      return ` - ${key}: ${objOneValue}\n`;
-    }
-
-    if (!objOneHasKey && objTwoHasKey) {
-      return ` + ${key}: ${objTwoValue}\n`;
-    }
-
-    if (objOneHasKey && objTwoHasKey) {
-      if (objOneValue === objTwoValue) {
-        return `   ${key}: ${objOneValue}\n`;
-      }
-    }
-
-    return ` - ${key}: ${objOneValue}\n + ${key}: ${objTwoValue}\n`;
-  });
-
-  const resultStr = resultArr.toString().replaceAll(',', '');
-
-  console.log(`{\n${resultStr}}`);
-
-  return `{\n${resultStr}}\n`;
+  console.log(stylish(diff));
 };
 
 export default genDiff;
