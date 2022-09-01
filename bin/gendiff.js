bd598fd6745005e7820592de909e0e860e82d977
Vladislav Sholokhov <vladislav.sholokhov@jetbrains.com>
2022-09-01 13:28:15 +0300

    sixth step: add stylish as default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,8 +7,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((file1, file2) => genDiff(file1, file2));
+  .action((file1, file2, options) => genDiff(file1, file2, options.format));
 
 program.parse(process.argv);
