4d68446a9571a792565955328c4919175f36e2ba
Vladislav Sholokhov <vladislav.sholokhov@jetbrains.com>
2022-10-08 16:50:46 +0300

    sixth 7: fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,23 +1,75 @@
 import { test, expect } from '@jest/globals';
-import path from 'path';
-import fs from 'fs';
 import genDiff from '../src/index.js';
 
-const getFilePath = (file) => path.resolve(process.cwd(), '__fixtures__', file);
-const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
+const stylishExpected = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
+
+const plainExpected = `Property 'common.follow' was added with value: false
+Property 'common.setting2' was removed
+Property 'common.setting3' was updated. From true to null
+Property 'common.setting4' was added with value: 'blah blah'
+Property 'common.setting5' was added with value: [complex value]
+Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.ops' was added with value: 'vops'
+Property 'group1.baz' was updated. From 'bas' to 'bars'
+Property 'group1.nest' was updated. From [complex value] to 'str'
+Property 'group2' was removed
+Property 'group3' was added with value: [complex value]`;
 
 test('check json files - stylish', () => {
-  expect(genDiff('file1.json', 'file2.json')).toEqual(readFile(getFilePath('expectedStylish.txt')));
+  expect(genDiff('file1.json', 'file2.json')).toEqual(stylishExpected);
 });
 
 test('check yaml files - stylish', () => {
-  expect(genDiff('file1.yaml', 'file2.yaml')).toEqual(readFile(getFilePath('expectedStylish.txt')));
+  expect(genDiff('file1.yaml', 'file2.yaml')).toEqual(stylishExpected);
 });
 
 test('check json files - plain', () => {
-  expect(genDiff('file1.json', 'file2.json', 'plain')).toEqual(readFile(getFilePath('expectedPlain.txt')));
+  expect(genDiff('file1.json', 'file2.json', 'plain')).toEqual(plainExpected);
 });
 
 test('check yaml files - plain', () => {
-  expect(genDiff('file1.yaml', 'file2.yaml', 'plain')).toEqual(readFile(getFilePath('expectedPlain.txt')));
+  expect(genDiff('file1.yaml', 'file2.yaml', 'plain')).toEqual(plainExpected);
 });
