85ae5fcbe87df665b7867d1a1387b20207b259a2
Vladislav Sholokhov <vladislav.sholokhov@jetbrains.com>
2023-06-22 18:54:04 +0300

    fix test

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -6,17 +6,18 @@ const buildDiffTree = (data1, data2) => {
   const uniqSortedKeys = _.sortBy(_.uniq([...keys1, ...keys2]));
 
   const buildTree = (obj1, obj2, key) => {
-    if (!_.has(obj1, key)) {
+    if (!_.has(obj2, key)) {
       return { name: key, type: 'removed', value: obj1[key] };
     }
 
-    if (!_.has(obj2, key)) {
+    if (!_.has(obj1, key)) {
       return { name: key, type: 'added', value: obj2[key] };
     }
 
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
       return { name: key, type: 'nested', children: buildDiffTree(obj1[key], obj2[key]) };
     }
+
     if (obj1[key] !== obj2[key]) {
       return {
         name: key, type: 'modified', previousValue: obj1[key], currentValue: obj2[key],
